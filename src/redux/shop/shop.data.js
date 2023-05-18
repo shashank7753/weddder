51939447d83e01420683a8f716b541f7bf427687
shashank7753 <89301898+shashank7753@users.noreply.github.com>
2023-05-18 10:35:38 +0530

    latest2

---
+++ src/redux/shop/shop.data.js
@@ -0,0 +1,207 @@
+const SHOP_DATA = {
+  hats: {
+    id: 1,
+    title: 'Photographers',
+    routeName: 'shoots',
+    items: [
+      {
+        id: 1,
+        name: 'Judson Campos',
+        imageUrl: 'https://i.ibb.co/sy7Z6mj/photo-1631621583126-ed10a80f62b1-crop-faces-edges-cs-tinysrgb-fit-crop-fm-jpg-ixid-Mnwx-Mj-A3f-DB8-M.png',
+        price: 4000
+      },
+      {
+        id: 2,
+        name: 'Sammie Walters',
+        imageUrl: 'https://i.ibb.co/Gc7PrfW/Cinque-Terre-Pre-Wedding-Shoot-039.png',
+        price: 5500
+      },
+      {
+        id: 3,
+        name: 'Trent Gilbert',
+        imageUrl: 'https://i.ibb.co/gRbk0pH/27ff99b9-b77f-474a-bd33-16706acc77d2big.png',
+        price: 12000 
+      },
+      {
+        id: 4,
+        name: 'Sang Roberts',
+        imageUrl: 'https://i.ibb.co/YLqx3Jx/raj-photo-and-video-8-15-248917-1567142929.png',
+        price: 2500
+      },
+      {
+        id: 5,
+        name: 'Ben Brown',
+        imageUrl: 'https://i.ibb.co/jJNzdQf/ATLHEA-STUDIO-1-scaled.png',
+        price: 1800
+      },
+      {
+        id: 6,
+        name: 'Derrick Riley',
+        imageUrl: 'https://i.ibb.co/BjjrpMC/ATLHEA-STUDIO-139-scaled.png',
+        price: 1400
+      },
+      {
+        id: 7,
+        name: 'Darnell Greer',
+        imageUrl: 'https://i.ibb.co/P1h5b1M/ATLHEA-OUTDOORSHOOT-43-scaled.png',
+        price: 1800
+      }
+    ]
+  },
+  sneakers: {
+    id: 2,
+    title: ' 4k Recording Photoshoot Package',
+    routeName: 'sneakers',
+    items: [
+      {
+        id: 10,
+        name: 'Fully Ready',
+        imageUrl: 'https://i.ibb.co/gtr4gnW/Kanyadaan1306.png',
+        price: 2200
+      },
+      {
+        id: 11,
+        name: 'Dhruv',
+        imageUrl: 'https://i.ibb.co/QM6dMr8/Wedding-Videography.png',
+        price: 2800
+      },
+      {
+        id: 12,
+        name: 'Divyanshu',
+        imageUrl: 'https://i.ibb.co/885Zx9x/wedding-videography-500x500.png',
+        price: 1100
+      },
+      {
+        id: 13,
+        name: 'distilled 4k',
+        imageUrl: 'https://i.ibb.co/RjT2r85/pre-wedding-cinematic-film-services-500x500.png',
+        price: 1600
+      },
+      {
+        id: 14,
+        name: 'Buttom 4k',
+        imageUrl: 'https://i.ibb.co/bPZJyfp/photo-1607968565043-36af90dde238-ixlib-rb-4-0.png',
+        price: 1600
+      },
+      {
+        id: 15,
+        name: ' Tops 4k',
+        imageUrl: 'https://i.ibb.co/frrDbg1/best-wedding-couple-portraits.png',
+        price: 160
+      },
+      {
+        id: 16,
+        name: 'Insight ',
+        imageUrl: 'https://i.ibb.co/dcvJg13/featured-840x480.png',
+        price: 1900
+      },
+      {
+        id: 17,
+        name: 'Timberlands',
+        imageUrl: 'https://i.ibb.co/fpn5Zhy/reception-entry.png',
+        price: 2000
+      }
+    ]
+  },
+  jackets:{
+    id: 3,
+    title: 'Mahurat Package',
+    routeName: 'jackets',
+    items: [
+      {
+        id: 18,
+        name: 'Wedding Main',
+        imageUrl: 'https://i.ibb.co/Y26wRbr/58231-bridal-couple-deepak-studios-lead.png',
+        price: 1250
+      },
+      {
+        id: 19,
+        name: 'Close Wed Main',
+        imageUrl: 'https://i.ibb.co/42SjS5R/1000-F-277855763-be-U5-DLF0x-Ks-Sv-Py36q-Tl-Kq-GAl7-N6-BI1c.png',
+        price: 90000
+      },
+      {
+        id: 20,
+        name: 'Varmala Ceremony',
+        imageUrl: 'https://i.ibb.co/MDmNnY6/dsc03723-15-83944-1552462284.png',
+        price: 9000
+      },
+      {
+        id: 21,
+        name: 'Brown Shearling',
+        imageUrl: 'https://cdn0.weddingwire.in/vendor/3944/3_2/1280/jpg/18194746-933747563432748-8988681507944494332-n_15_83944.webp',
+        price: 1650
+      },
+      {
+        id: 22,
+        name: 'Kanyadan',
+        imageUrl: 'https://i.ibb.co/gtr4gnW/Kanyadaan1306.png',
+        price: 1850
+      }
+    ]
+  },
+  womens: {
+    id: 4,
+    title: 'Non Profit Events',
+    routeName: 'womens',
+    items: [
+      {
+        id: 1,
+        name: 'Judson Campos',
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1683121341746-defea7bfc148?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80',
+        price: 4000
+      },
+      {
+        id: 2,
+        name: 'Sammie Walters',
+        imageUrl: 'https://images.unsplash.com/photo-1593113598332-cd288d649433?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        price: 5500
+      },
+      {
+        id: 3,
+        name: 'Trent Gilbert',
+        imageUrl: 'https://images.unsplash.com/photo-1599059813005-11265ba4b4ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        price: 12000 
+      },
+      {
+        id: 4,
+        name: 'Sang Roberts',
+        imageUrl: 'https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        price: 2500
+      }, 
+    ]
+  },
+  mens: {
+    id: 5,
+    title: 'Institutional Events',
+    routeName: 'mens',
+    items: [
+      {
+        id: 11,
+        name: 'Farewell',
+        imageUrl: 'https://images.unsplash.com/photo-1529787730-bdcabd22a644?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        price: 2800
+      },
+      {
+        id: 12,
+        name: 'Seminars',
+        imageUrl: 'https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        price: 1100
+      },
+      {
+        id: 13,
+        name: 'Competition',
+        imageUrl: 'https://images.unsplash.com/flagged/photo-1576448438685-9f5e5b283d4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        price: 1600
+      },
+      {
+        id: 14,
+        name: 'Sports Day',
+        imageUrl: 'https://images.unsplash.com/photo-1552034350-f9a94480c6e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        price: 1600
+      }, 
+    ]
+  }
+};
+
+export default SHOP_DATA;
