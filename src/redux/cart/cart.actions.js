a27939e49354580686933598dc3641c74833eb26
shashank7753 <89301898+shashank7753@users.noreply.github.com>
2024-03-03 22:34:54 +0530

    menu update

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,20 +1,44 @@
+// import CartActionTypes from "./cart.types";
+
+// export const toggleCartHidden = () => ({
+//     type: CartActionTypes.TOGGLE_CART_HIDDEN
+// })
+
+// export const addItem = item => ({
+//     type: CartActionTypes.ADD_ITEM,
+//     payload: item
+// })
+
+// export const clearItemFromCart = item => ({
+//     type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+//     payload: item
+// })
+
+// export const removeItem = item => ({
+//     type: CartActionTypes.REMOVE_ITEM,
+//     payload: item
+// })
 import CartActionTypes from "./cart.types";
 
+// Action creator for toggling cart hidden state
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
+});
 
+// Action creator for adding an item to the cart
 export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
     payload: item
-})
+});
 
+// Action creator for clearing an item from the cart
 export const clearItemFromCart = item => ({
     type: CartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item
-})
+});
 
+// Action creator for removing an item from the cart
 export const removeItem = item => ({
     type: CartActionTypes.REMOVE_ITEM,
     payload: item
-})
+});
