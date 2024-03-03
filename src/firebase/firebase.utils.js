3ce96888c1b5bb070c59a17551eca4a49347272a
shashank7753 <89301898+shashank7753@users.noreply.github.com>
2024-03-03 21:38:13 +0530

    ..

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,9 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+
+
+console.log(process.env.REACT_APP_API_KEY)
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
